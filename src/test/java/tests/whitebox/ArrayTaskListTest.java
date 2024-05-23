2d7546d2d9ac73be037edbf9299fae9d22730352
Stefan Nastasa <stefan21nastasa@gmail.com>
2024-05-23 19:18:44 +0300

    Add surefire plugin

--- src/test/java/tests/whitebox/ArrayTaskListTest.java
+++ src/test/java/tests/whitebox/ArrayTaskListTest.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class ArrayTaskListTest {
+public class ArrayTaskListTest {
     private ArrayTaskList atl;
     private Date date;
 
